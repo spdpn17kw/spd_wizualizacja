da6ffd483db231d684ed011133b982a8327e9b40
Weronika <wlisowska7@gmail.com>
2019-03-11 14:06:56 +0100

    wszystko dziala

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,33 +1,24 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 import static java.lang.Integer.max;
 
 public class Main extends Application {
-
-    private TableView<Node> table = new TableView<Node>();
+    public static int Cmax = 10;
+    public static int n_m,n_ex;
+    private static int W,H;
 
     public static void main(String[] args) {
         launch(args);
@@ -35,14 +26,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws FileNotFoundException {
-        int Cmax = 10;
-        int n_ex = 0,n_m = 0;
+        String[] list_colors = {"X","A","C","S","W","O","E","T","U"};
         String color = "";
-
-        Scene scene = new Scene(new Group());
-        stage.setTitle("Wizualizacja");
-        stage.setWidth(450);
-        stage.setHeight(500);
         File plik = new File("C:\\Users\\Weronika\\source\\repos\\johnson_ja\\johnson_ja\\wiz.txt");
         Scanner odczyt = new Scanner(plik);
         String text = odczyt.nextLine();
@@ -55,8 +40,7 @@ public class Main extends Application {
                 macierz[j][i] = new Task();
                 text = odczyt.nextLine();
                 macierz[j][i].duration = Integer.parseInt(text);
-                String st = Integer.toString(j+1);
-                macierz[j][i].color = st;
+                macierz[j][i].color = list_colors[j];
             }
             System.out.println();
         }
@@ -72,45 +56,33 @@ public class Main extends Application {
         }
         text = odczyt.nextLine();
         Cmax = Integer.parseInt(text);
-        Node[][] nodes = new Text[n_m][Cmax];
+        W = 20*Cmax;
+        H = 20*(n_m+1);
+        Tab tab = new Tab();
+        Node[][] nodes = new Text[n_m+1][Cmax];
+        nodes = tab.give_nodes0(Cmax,n_m);
+
+        stage.setTitle("Wizualizacja");
 
         for(int j = 0;j<n_m;j++){
             int k=0;
             for(int i=0;i<n_ex;i++){
-
-                    String tekst = macierz[i][j].color;
-                System.out.println(tekst);
-                //if(i==n_ex-1){
-                    for(int dur=0;dur<macierz[i][j].duration;dur++)nodes[j][k] = new Text(tekst);
-//                }
-//                    while(k<macierz[i+1][j].time_of_start){
-//                        nodes[j][k] = new Text(tekst);
-//                        k++;
-//                    }
+                String tekst = macierz[i][j].color;
+                    for(int dur=0;dur<macierz[i][j].duration;dur++){
+                        for(int z=k;z<=macierz[i][j].time_of_start;z++){
+                            k=z;
+                        }
+                        nodes[j+1][k] = new Text(tekst);
+                        k++;
+                    }
             }
         }
 
         final Label label = new Label("Wizualizacja");
         label.setFont(new Font("Arial", 20));
-        TableColumn[] tab = new TableColumn[Cmax];
-        table.setEditable(true);
-        for(int i=1;i<=Cmax;i++){
-            String j = Integer.toString(i);
-            tab[i-1] = new TableColumn(j);
-            tab[i-1].setPrefWidth(20);
-            tab[i-1].setCellValueFactory(new PropertyValueFactory(j));
-        }
 
-        table.getColumns().addAll(tab);
-
-        table.getItems().addAll(nodes[0]);
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, table);
-
-        ((Group) scene.getRoot()).getChildren().addAll(vbox);
-        System.out.println("dsvcsdcsdcsdc");
+        tab.give_nodes1(nodes,Cmax,n_m);
+        Scene scene = new Scene(tab.getRoot(),W,H);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/sample/Tab.java
@@ -0,0 +1,51 @@
+package sample;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class Tab {
+    public static int Cmax = 50;
+    public static int n_m;
+    private Group root = new Group();
+
+    public Node[][] give_nodes0(int cmax, int m) {
+        Cmax = cmax;
+        n_m = m;
+        Node[][] nodes = new Text[n_m+1][Cmax];
+        for(int i = 0; i<n_m+1;i++)
+            for(int j=0;j<Cmax;j++) {
+                if(i == 0) nodes[i][j] = new Text(Integer.toString(j+1));
+
+                else nodes[i][j] = new Text(" ");
+            }
+            return nodes;
+    }
+    public void give_nodes1(Node[][] nodes, int cmax, int m) {
+        Cmax = cmax;
+        n_m = m;
+        Node[] tab_node = new Text[Cmax*(n_m+1)];
+        for(int j=0;j<Cmax;j++) {
+            tab_node[j] = nodes[0][j];
+            tab_node[j].setLayoutX(j*20);
+            tab_node[j].setLayoutY(1*20);
+            System.out.println(tab_node[j]);
+        }
+        for(int i = 1; i<n_m+1;i++)
+            for(int j=0;j<Cmax;j++){
+                tab_node[j+Cmax*i] = nodes[i][j];
+                tab_node[j+Cmax*i].setLayoutX(j*20);
+                tab_node[j+Cmax*i].setLayoutY((i+1)*20);
+                System.out.println(tab_node[j+Cmax*i]);
+                System.out.println(tab_node[j]);
+            }
+        root = new Group(tab_node);
+    }
+
+    public Group getRoot() {
+        return root;
+    }
+
+}
